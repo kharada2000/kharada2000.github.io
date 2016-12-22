2d63e4066cd3d4ad9015c0740bb0294a4b759d26
kharada2000 <kazmagic@gmail.com>
2016-12-22 11:01:36 +0900

    12/22

---
+++ README.md
@@ -0,0 +1,4 @@
+# support tool for making time entry
+
+test5.js - PTO helper only
+
--- test6.js
+++ test6.js
@@ -1,14 +1,50 @@
 /* feedback to Time Entry window */
 function pushDate(){
+	if(pto.length==0){
+		if(window.confirm('No PTO?'))alert('ok');
+	}		
 	for(var i=0;i<pto.length;i++){
 		for(var j=1;j<ptorow;j++){
 			setValue(pto[i]-1,j,null,null,null);
 		}
 		setValue(pto[i]-1,ptorow,null,null,'7.5');
 	}
-	alert('test');
 	w.close();
 }
+
+function temp(){
+	/* for a month */
+	for(x=0;x<31;x++){
+		if(document.getElementById('B24_1_'+x)==null){break}
+		if(weekends.indexOf(x+1)>=0){
+			setValue(x,1,null,null,null);
+			setValue(x,2,null,null,null);
+			setValue(x,3,null,null,null);
+			continue
+		}
+		if(holidays.indexOf(x+1)>=0){
+			setValue(x,1,null,null,8);
+			continue
+		}
+		var time1 = randTime('09:00',60,0);
+		var time2 = randTime('11:30',60,0);
+		var time3 = randTime(time2,30,40);
+		var time4 = randTime('19:00',180,0);
+		if (holidays.length==0){
+			setValue(x,1,time1,time2,null);
+			setValue(x,2,time2,time3,null);
+			setValue(x,3,time3,time4,null);
+		}else{
+			setValue(x,1,null,null,null);
+			setValue(x,2,time1,time2,null);
+			setValue(x,3,time2,time3,null);
+			setValue(x,4,time3,time4,null);
+		}
+	}
+	submitForm('DefaultFormName',1,{'_FORM_SUBMIT_BUTTON':recalcId});
+	alert('Finished!');
+}
+
 
 /* select multiple PTO date */
 function selectDate(id){
@@ -59,6 +95,35 @@ function getOffdays(_date){
 	return days;
 }
 
+function getHolidays(date){
+	if(!date.getMonth){return false}
+	switch (date.getMonth() +1){
+		case 1:return [1,2,3,9];
+		case 2:return [11];
+		case 3:return [20];
+		case 4:return [29];
+		case 5:return [3,4,5];
+		case 7:return [17];
+		case 8:return [11];
+		case 9:return [18,23];
+		case 10:return [9];
+		case 11:return [3,23];
+		case 12:return [23,30,31]
+	}
+	return []
+}
+function getWeekends(_date){
+	var weekends=[];
+	var month=_date.getMonth();
+	var date=new Date(_date);date.setDate(1);
+	for (;date.getMonth()==month;date.setDate(date.getDate()+1)){
+		if (date.getDay()==0 || date.getDay()==6){
+			weekends.push(date.getDate())
+		}
+	}
+	return weekends
+}
+
 var myDate=new Date(document.getElementById('N56').value.split('|')[0]);
 var myWeekTbl="SMTWTFS";
 var myMonthTbl=[31,28,31,30,31,30,31,31,30,31,30,31];
@@ -71,6 +136,14 @@ myDate.setDate(1);
 var myWeek = myDate.getDay();
 var myTblLine = Math.ceil((myWeek+myMonthTbl[myMonth])/7);
 var myTable = new Array(7*myTblLine);
+
+var addrowId=document.documentElement.outerHTML.match(/addrow[^\']+/);
+var recalcId=document.documentElement.outerHTML.match(/recalc[^\']+/);
+var config=[{work_type:'Public Holiday'},{work_type:'Regular Hours'},{work_type:'Break'},{work_type:'Regular Hours'}];
+var holidays=getHolidays(first_day);
+var weekends=getWeekends(first_day);
+if (holidays.length==0){config.shift();}
+
 
 for(i=0; i<7*myTblLine; i++) myTable[i]="";
 for(i=0; i<myMonthTbl[myMonth]; i++) myTable[i+myWeek]=i+1;
